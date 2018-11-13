706c73af70ddd722441ebab1035c59bb26dd1133
lehongdanh99 <43432402+lehongdanh99@users.noreply.github.com>
2018-11-13 16:30:13 +0700

    link mới
    mới

---
+++ alo/AnhVIet/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Speech.Synthesis;
+
+namespace AnhVIet
+{
+    public partial class Form1 : Form 
+    {
+       
+       
+        NODE node , root= new NODE();
+        Tree tree = new Tree();
+        string T = "admin";
+        SpeechSynthesizer read;  //âm thanh
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String Line;
+            StreamReader sr = new StreamReader("D:\\Tudien.txt");
+            Line = sr.ReadLine();
+            while (Line != null)
+            {
+                string[] words = Line.Split(':');       //Slpit tách chuổi thành 2 chuổi nhỏ bới dấu hai chấm
+                node = tree.Insert(node, words[0], words[1]);
+                Line = sr.ReadLine();
+            }
+            root = node;
+            sr.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Form2 add = new Form2();
+            add.ShowDialog();
+           
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            groupBox1.Show();
+            Tree a = new Tree();
+            a.Del(ref root, "load");
+            textBox1.Text = "";
+            txtNghiaTu.Text = "";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           if( MessageBox.Show("Bạn có muốn thoát khỏi chương trình ?", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel=true;
+            }
+            System.GC.Collect();
+        }
+
+        private void btnDN_Click(object sender, EventArgs e)
+        {
+            if(txtTK.Text==T && txtMK.Text==T)
+            {
+                MessageBox.Show("Đăng nhập thành công");
+                Form3 f3 = new Form3();
+                f3.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Đã nhập sai thông tin đăng nhập","Thông báo",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            read = new SpeechSynthesizer();
+            read.SpeakAsync(textBox1.Text);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+           
+            if (tree.Seacrch(root, textBox1.Text) != null)
+            {
+                txtNghiaTu.Text = tree.Seacrch(root, textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("Không có từ này! Bạn có thể thêm từ ^^");
+            }
+            Dispose();
+        }      
+    }
+}
---
+++ alo/AnhVIet/Form2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnhVIet
+{
+    public partial class Form2 : Form
+    {
+        String Line;
+        NODE node = null, root = new NODE();
+        Tree tree = new Tree();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void MeanBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WordBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.GC.Collect();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (MeanBox.Text == "" || WordBox.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập đủ nghĩa và từ! ", "THÔNG BÁO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                StreamReader sr = new StreamReader("D:\\Tudien.txt");
+                Line = sr.ReadLine();
+                while (Line != null)
+                {
+                    string[] words = Line.Split(':');       //Slpit tách chuổi thành 2 chuổi nhỏ bới dấu hai chấm
+                                                            //node = tree.Insert(ref node, words[0], words[1]);
+                    tree.Insert(root, words[0], words[1]);
+                    Line = sr.ReadLine();
+                }
+                root = node;
+                sr.Close();
+
+                string fword = WordBox.Text;
+                if (tree.Seacrch(root, fword) != null)
+                {
+                    MessageBox.Show("Từ đã có trong từ điển! ");
+                }                                
+                else
+                {
+                        //Pass the filepath and filename to the StreamWriter Constructor
+                        StreamWriter sw = new StreamWriter("D:\\Tudien.txt", true);
+
+                        //Write a line of text
+                        sw.WriteLine(WordBox.Text + ":" + MeanBox.Text);
+                        sw.Close();
+                        MessageBox.Show("Them Thanh Cong! ");
+                }
+            }
+        }
+    }
+}
+
